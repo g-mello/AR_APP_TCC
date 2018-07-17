2ffdc3d9b7f7f7dd4019b51c30d5653d8ee4f295
Guilherme Mello <esquilogp@gmail.com>
2018-07-16 21:55:01 -0300

    Add Vurmark DB, Add Controller Script

--- Assets/Scripts/Controller.cs
+++ Assets/Scripts/Controller.cs
@@ -1,16 +1,34 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using Vuforia;
 
-public class Controller : MonoBehaviour {
+public class Controller : MonoBehaviour
+{
 
-	// Use this for initialization
-	void Start () {
-		
-	}
-	
-	// Update is called once per frame
-	void Update () {
-		
-	}
+    private VuMarkManager mVuMarkManager;
+    private int targetID;
+
+
+    void Start()
+    {
+        mVuMarkManager = TrackerManager.Instance.GetStateManager().GetVuMarkManager();
+    }
+
+
+    void Update()
+    {
+
+        //Get the current Target ID from the target has been tracked
+        foreach (var vmark in mVuMarkManager.GetActiveBehaviours())
+        {
+
+            targetID = System.Convert.ToInt32(vmark.VuMarkTarget.InstanceId.NumericValue);
+
+            Debug.Log(message: "Target ID: " + targetID);
+        }
+
+
+
+    }
 }
