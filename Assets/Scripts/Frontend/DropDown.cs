b19701f0f4f2c82e6ccd9e42f7b4e8e7ed7b6849
Guilherme Mello <esquilogp@gmail.com>
2018-09-22 15:44:31 -0300

    Fix How to Plant UI Overlay

--- Assets/Scripts/Frontend/DropDown.cs
+++ Assets/Scripts/Frontend/DropDown.cs
@@ -7,22 +7,13 @@ public class DropDown : MonoBehaviour {
 
     //State 
     public UnityEngine.UI.Dropdown dropdown_state;
-    List<string> states = new List<string>() { "Estado", "Acre", "Alagoas","Amapá", "Amazonas", "Bahia",
-                                               "Ceará", "Distrito Federal", "Goiás", "Maranhão", "Mato Grosso",
-                                               "Mato Grosso do Sul", "Minas Gerais", "Pará", "Paraíba", "Paraná",
-                                               "Pernambuco", "Piauí", "Rio de Janeiro", "Rio Grande do Norte",
-                                               "Rio Grande do Sul", "Rondônia", "Roraima", "Santa Catarina", "São Paulo",
-                                               "Sergipe", "Tocantins"
-                                                };
+    List<string> states = new List<string>() { "Estado", "Minas Gerais", "Rio de Janeiro", "São Paulo"};
     
     // City
     public UnityEngine.UI.Dropdown dropdown_city;
-    List<string> cities_sp = new List<string>() { "Cidade","Araraquara", "Barretos", "Batatais", "Bauru",
-                                                "Campinas", "Campos do Jordão", "Cedral", "Diadema", "Dracena", "Dumont",
-                                                "Embu", "Franca", "Francisco Morato","Guará","Guaíra", "Holambra", "Hortolândia",
-                                               "Ibitinga", "Indaiatuba", "Igarapaga","Jaboticabal", "Jadinópolis", "Jaú",
-                                               "Leme", "Marília", "Matão", "Osasco", "Piracicaba", "Presidente Prudente",
-                                               "Ribeirão Preto", "Rio Claro", "Santos", "São Carlos", "São Paulo", "Taubaté"}; 
+    List<string> cities_sp = new List<string>() { "Cidade","Araraquara", "Barretos", "Batatais", 
+                                                "Campinas", "Franca", "Holambra", 
+                                               "Ribeirão Preto", "São Carlos"}; 
 
     // Garden Center
     public UnityEngine.UI.Dropdown dropdown_garden_center;
